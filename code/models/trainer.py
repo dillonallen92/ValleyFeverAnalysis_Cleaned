24920c76341863a2b183ae01bd5b55f3692dc916
dillonallen92 <dillon.allen.92@gmail.com>
2025-11-28 21:17:35 -0800

    trainer code created

---
+++ code/models/trainer.py
@@ -0,0 +1,32 @@
+import torch
+
+class Trainer:
+    def __init__(self, model, criterion, optimizer, scaler_y):
+        self.model = model
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.scaler_y = scaler_y
+
+    def train(self, X_train, y_train, epochs):
+        for epoch in range(epochs):
+            self.model.train()
+
+            preds = self.model(X_train)
+            loss = self.criterion(preds, y_train)
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+        return
+
+    def evaluate(self, X_test, y_test):
+        self.model.eval()
+        with torch.no_grad():
+            preds = self.model(X_test).cpu().numpy()
+            true  = y_test.cpu().numpy()
+
+        return (
+            self.scaler_y.inverse_transform(preds),
+            self.scaler_y.inverse_transform(true),
+        )
