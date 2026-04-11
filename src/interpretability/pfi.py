fixed error in pfi masking

src/interpretability/pfi.py
@@ -26,9 +26,11 @@ def permutation_feature_importance(model, X_test, y_test, mask, scaler_y, metric
       X_perm = X_test.clone()
       idx = torch.randperm(X_test.shape[1], generator = g)
       X_perm[:,:,feat] = X_test[:, idx, feat]
+      mask_perm = mask.clone()
+      mask_perm[:, idx, feat] = mask[:, feat]
       
       with torch.no_grad():
-        preds = model(X_perm, mask).cpu().numpy().reshape(-1, 1)
+        preds = model(X_perm, mask_perm).cpu().numpy().reshape(-1, 1)
         preds = scaler_y.inverse_transform(preds).flatten()
       
       err = metric_fn(base_true, preds)